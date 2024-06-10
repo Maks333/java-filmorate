f8479f698735b87f170a1c5d7b99fcb43db37373
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-10 15:05:01 +0700

    Create tests for GET /users endpoint

--- src/test/java/ru/yandex/practicum/filmorate/model/UserModelTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserModelTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserModelTest {
@@ -16,10 +20,28 @@ public class UserModelTest {
 
     //GET tests
     @Test
-    public void getEmptyCollectionTest() {}
+    public void getEmptyCollectionTest() {
+        Collection<User> users = controller.getUsers();
+        assertNotNull(users, "Collection is null");
+        assertEquals(0, users.size(), "Size is not 0");
+    }
 
     @Test
-    public void getNotEmptyCollectionTest() {}
+    public void getNotEmptyCollectionTest() {
+        User user = new User();
+        user.setEmail("email@email.ru");
+        user.setLogin("login");
+        user.setName("username");
+        user.setBirthday(LocalDate.of(1999, Month.MAY, 18));
+
+        User addedUser = controller.create(user);
+        assertNotNull(addedUser, "User is not added");
+        assertEquals(1, addedUser.getId(), "User has incorrect id");
+
+        Collection<User> users = controller.getUsers();
+        assertEquals(1, users.size(), "Collection contains " + users.size() + " users");
+        assertTrue(users.contains(addedUser), "User in collection is not equal to added user");
+    }
 
     //POST tests
     @Test

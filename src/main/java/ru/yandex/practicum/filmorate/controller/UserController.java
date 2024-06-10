fe493abb84876ab076e15502751c4e9e8dc0166e
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-10 14:24:44 +0700

    Create POST /users endpoint

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +21,10 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody User user) {
-        return null;
+        validate(user);
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
     }
 
     @PutMapping
@@ -30,13 +35,33 @@ public class UserController {
 
     private void validate(User user) {
         //user cannot be null
+        if (user == null) {
+            throw new ValidationException("User object reference to null");
+        }
         //email cannot be null or empty and must contain @ character
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Email is empty or does not contain '@' character");
+        }
         //login cannot be null or empty or contain space characters
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Login contains space characters or is empty");
+        }
         //If name is empty assign login to name field else name
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
         //date of birth cannot be in future
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Birthday cannot be in the future");
+        }
     }
 
     private int getNextId() {
-        return 0;
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
     }
 }

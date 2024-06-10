a2f6a133ec0863df0aaf49d583da5a17f235eaab
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-10 12:21:25 +0700

    Create tests for GET /films endpoint.

--- src/test/java/ru/yandex/practicum/filmorate/model/FilmModelTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmModelTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collection;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FilmModelTest {
     private FilmController controller;
 
@@ -15,9 +21,28 @@ public class FilmModelTest {
 
     //GET tests
     @Test
-    public void getEmptyCollectionTest() {}
+    public void getEmptyCollectionTest() {
+        Collection<Film> films = controller.getFilms();
+        assertNotNull(films, "Collection is null");
+        assertEquals(0, films.size(), "Size is not 0");
+    }
+
     @Test
-    public void getNotEmptyCollectionTest() {}
+    public void getNotEmptyCollectionTest() {
+        Film film = new Film();
+        film.setName("FilmName");
+        film.setDescription("FilmDescription");
+        film.setDuration(Duration.ofMinutes(120));
+        film.setReleaseDate(LocalDate.of(1999, Month.MARCH, 22));
+
+        Film addedFilm = controller.create(film);
+        assertNotNull(addedFilm, "Film is not added");
+        assertEquals(1, addedFilm.getId(), "Film has incorrect id");
+
+        Collection<Film> films = controller.getFilms();
+        assertEquals(1, films.size(), "Collection contains " + films.size() + " films");
+        assertTrue(films.contains(addedFilm), "Film in collection not equal to added film");
+    }
 
     //POST tests
     @Test

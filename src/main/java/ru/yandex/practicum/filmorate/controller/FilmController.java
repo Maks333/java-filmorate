f78aeede7f5ee2d6e8ad2d884252df064f14e790
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-10 08:29:25 +0700

    Create GET /films endpoint

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,19 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
 }

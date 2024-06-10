bff9319cfc17952aa8c2c791af5b2dde43ba2f31
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-10 15:37:09 +0700

    Add context load test for controllers

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,23 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class FilmorateApplicationTests {
+    @Autowired
+    FilmController filmController;
+    @Autowired
+    UserController userController;
 
     @Test
     void contextLoads() {
+        assertNotNull(filmController, "Not loading properly");
+        assertNotNull(userController, "Not loading properly");
     }
-
 }

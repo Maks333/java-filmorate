0ba0cc1f1a91ac549bf2be42e6f357ae1815ae63
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-22 18:24:28 +0700

    Create interface for UserService class

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,7 +1,35 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class UserService {
+    private final UserStorage storage;
+
+
+    public List<User> getCommonFriends(long id, long otherId) {
+        return List.of();
+    }
+
+    public List<User> getFriends(long id) {
+        return null;
+    }
+
+    public void deleteFried(long id, long friendId) {
+
+    }
+
+    public void addFriend(long id, long friendId) {
+
+    }
+
+    public User getUser(long id) {
+        return null;
+    }
 }

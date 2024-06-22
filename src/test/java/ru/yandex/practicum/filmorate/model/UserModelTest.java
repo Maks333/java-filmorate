d8cc18de2e1683ec998a69572919404e9818d7b8
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-22 18:24:12 +0700

    Adjust user model tests to new class structure

--- src/test/java/ru/yandex/practicum/filmorate/model/UserModelTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserModelTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
@@ -15,12 +16,13 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class UserModelTest {
-    private UserController controller;
+    UserController controller;
 
     @BeforeEach
     public void beforeEach() {
-        controller = new UserController(new InMemoryUserStorage());
+        controller = new UserController(new InMemoryUserStorage(), new UserService(new InMemoryUserStorage()));
     }
 
     //GET tests

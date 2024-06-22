e338ee9f0dcf0db4685f3a713075e4e12a44087b
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-22 18:09:25 +0700

    Create complete web API for UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.List;
@@ -12,10 +13,45 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserStorage storage;
+    private final UserService service;
 
     @GetMapping
     public List<User> getUsers() {
         return storage.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable long id) {
+        return service.getUser(id);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(
+            @PathVariable long id,
+            @PathVariable long friendId
+    ) {
+        service.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(
+            @PathVariable long id,
+            @PathVariable long friendId
+    ) {
+        service.deleteFried(id, friendId);
+    }
+
+    @GetMapping("{id}/friends")
+    public List<User> getFriends(@PathVariable long id) {
+        return service.getFriends(id);
+    }
+
+    @GetMapping("{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(
+            @PathVariable long id,
+            @PathVariable long otherId
+    ) {
+        return service.getCommonFriends(id, otherId);
     }
 
     @PostMapping

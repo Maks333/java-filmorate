24942393d715f73aec96ff7c9171c47ec25d9c46
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-23 10:47:35 +0700

    Implement likeFilm function

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,15 +3,17 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class FilmService {
-    private final FilmStorage storage;
-    private final UserService userService;
+    private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
 
     public List<Film> getFilmsByLikes(long count) {
         return null;
@@ -22,22 +24,24 @@ public class FilmService {
     }
 
     public void likeFilm(long id, long userId) {
-
+        User user = userStorage.getUserById(userId);
+        Film film = filmStorage.getFilmById(id);
+        film.getLikes().add(user.getId());
     }
 
     public Film getFilmById(long id) {
-        return storage.getFilmById(id);
+        return filmStorage.getFilmById(id);
     }
 
     public List<Film> getFilms() {
-        return storage.getFilms();
+        return filmStorage.getFilms();
     }
 
     public Film create(Film film) {
-        return storage.create(film);
+        return filmStorage.create(film);
     }
 
     public Film update(Film film) {
-        return storage.update(film);
+        return filmStorage.update(film);
     }
 }

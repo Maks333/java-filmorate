49c9e0e0786ccad18401ae8b75c15e27949d564d
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-07-26 08:02:04 +0700

    Create tests for create/update methods

--- src/test/java/ru/yandex/practicum/filmorate/dbstorages/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dbstorages/FilmDbStorageTest.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.jdbc.Sql;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 
@@ -38,5 +41,44 @@ public class FilmDbStorageTest {
 
         assertThat(films)
                 .size().isEqualTo(5);
+    }
+
+    @Test
+    public void testFilmCreation() {
+        Film film = new Film();
+        film.setName("Film10");
+        film.setDescription("Film10Desc");
+        film.setDuration(Duration.ofMinutes(30));
+        film.setReleaseDate(LocalDate.of(1999, 5, 10));
+        film.setRating(2L);
+        film.setGenres(new HashSet<>(List.of(1L, 2L, 3L)));
+
+        film = filmStorage.create(film);
+        assertThat(film)
+                .hasFieldOrPropertyWithValue("id", 6L);
+
+        Film filmFromDb = filmStorage.getFilmById(film.getId());
+        assertThat(filmFromDb)
+                .hasFieldOrPropertyWithValue("id", 6L);
+        System.out.println(filmFromDb);
+        System.out.println(filmFromDb.getDuration().toMinutes());
+    }
+
+    @Test
+    public void testFilmUpdate() {
+        Film filmFromDb = filmStorage.getFilmById(1);
+        System.out.println(filmFromDb);
+
+        Film film = new Film();
+        film.setName("Film10");
+        film.setDescription("Film10Desc");
+        film.setDuration(Duration.ofMinutes(30));
+        film.setReleaseDate(LocalDate.of(1999, 5, 10));
+        film.setRating(2L);
+        film.setGenres(new HashSet<>(List.of(1L)));
+        film.setId(1);
+        Film updatedFilm = filmStorage.update(film);
+
+        System.out.println(updatedFilm);
     }
 }

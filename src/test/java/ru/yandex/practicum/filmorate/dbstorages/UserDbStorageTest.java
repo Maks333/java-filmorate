dfcdb817025f85d59e0b34fe8c9d53eb355bac0c
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-07-26 08:40:18 +0700

    Create testFindUserById

---
+++ src/test/java/ru/yandex/practicum/filmorate/dbstorages/UserDbStorageTest.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.dbstorages;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.ComponentScan;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@AutoConfigureTestDatabase
+@JdbcTest
+@ComponentScan("ru.yandex.practicum.filmorate")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class UserDbStorageTest {
+    private final UserDbStorage userStorage;
+
+    @Test
+    public void testFindUserById() {
+        User user = userStorage.getUserById(1);
+
+        assertThat(user)
+                .hasFieldOrPropertyWithValue("id", 1L);
+        System.out.println(user);
+    }
+}
